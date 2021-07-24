2b5acc04f93319b25c11b8989ee71cf5d51cc833
shehan-sandaruwan <shehan.k.dinuka@gmail.com>
2021-07-24 22:48:56 +0530

    boiler plate for React Typescript project with babel and webpack with other formatting tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import './styles.css'
+import IMAGE from './react-logo.png'
+import LOGO from './reactjs-icon.svg'
+import { ClickCounter } from './Counter'
+
 export const App = () => {
-    return <h1>React TypeScript Webpack Starter Template</h1>
+  const num = 'shehan'
+
+  return (
+    <>
+      <h1>
+        Edit Hello React TypeScript Webpack Starter Template -{' '}
+        {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="300" />
+      <img src={LOGO} alt="React Logo" width="300" />
+      <ClickCounter />
+    </>
+  )
 }
